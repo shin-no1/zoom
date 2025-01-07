909269a416b295db2a3dc007095e4de1bcd78d37
shin-no1 <shinhe524@gmail.com>
2025-01-07 12:30:59 +0900

    feat: pug, static 설정 추가

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import * as path from "node:path";
 
 const app = express();
 
-console.log("hello world");
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log("Listening on http://localhost:3000");
+app.listen(3000, handleListen);
